09cbe5bf27d2b09f1db2651fd185b5cbdde99e84
Abdellah Ahmed <24692961+abdellahahmed@users.noreply.github.com>
2019-02-13 14:21:39 +0600

    routes created

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// get a list of taxi from database
+router.get('/taxi', function(req, res) {
+    res.send({
+        type: 'GET'
+    });
+});
+
+// add a new taxi to database
+router.post('/taxi', function(req, res) {
+    res.send({
+        type: 'POST'
+    });
+});
+
+// update a taxi in the database
+router.put('/taxi/:id', function(req, res) {
+    res.send({
+        type: 'PUT'
+    });
+});
+
+
+// delete a taxi from database
+router.delete('/taxi/:id', function(req, res) {
+    res.send({
+        type: 'DELETE'
+    });
+});
+
+
+module.exports = router;
